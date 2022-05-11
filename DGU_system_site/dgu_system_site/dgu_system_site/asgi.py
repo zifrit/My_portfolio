94797e9f93a00ab9415cebb29d72a2f4cfb77474
zifrit <zifrit@list.ru>
2022-05-11 23:59:06 +0300

    создание и подключение базы

--- DGU_system_site/dgu_system_site/dgu_system_site/asgi.py
+++ DGU_system_site/dgu_system_site/dgu_system_site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dgu_sistem_site project.
+ASGI config for dgu_system_site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dgu_sistem_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dgu_system_site.settings')
 
 application = get_asgi_application()
--- DGU_system_site/dgu_system_site/dgu_system_site/urls.py
+++ DGU_system_site/dgu_system_site/dgu_system_site/urls.py
@@ -1,4 +1,4 @@
-"""dgu_sistem_site URL Configuration
+"""dgu_system_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('dgu_students.urls'))
 ]
